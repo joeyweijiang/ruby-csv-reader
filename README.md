06dd15c9e4fed16a448031c96404cb039256ec2a
Joey <joey@Lees-iMac.local>
2017-02-23 16:13:01 -0600

    Add reading and dumping parts

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# test project for using ruby lib
+# Test project for using ruby open lib
+# For more lib information, please check: https://github.com/jpatokal/script_detector
+# Running script to read csv file and detect if words are traditional or not.
+# As a output file, it stores all traditional words. 
+
--- reader.rb
+++ reader.rb
@@ -1,7 +1,6 @@
 require 'csv'
-##require './lib/lib/chinese_detector'
-##require './lib/lib/script_detector'
-puts "CSV Reader"
+require File.dirname(__FILE__) + '/lib/lib/script_detector.rb'
+puts "CSV Reader Script Start:"
 
 class Options
     def initialize(input, output)
@@ -16,12 +15,27 @@ class Options
     end
 end
 
-
-options = Options.new(ARGV[0], ARGV[1])
-puts "#{options.input} >> #{options.output}"
-
-file = File.open(options.input,"r:ISO-8859-1")
-CSV.foreach(file) do |row|
-    puts "#{row}"
+if ARGV.size < 2
+    puts "Error: Script needs 2 parameters as input file and output file.\n try: ruby reader.rb input.csv output.txt\n"
+    abort
 end
+
+options = Options.new(ARGV[0], ARGV[1])             # Get input and output
+outAry = Array.new                                  # Array for output
+puts "Reading words from #{options.input} >> #{options.output}"    
+
+file = File.open(options.input,"r:ISO-8859-1")      # Read file in default format in case
+CSV.foreach(file) do |row|                          # Check each name column words
+    next if row.size < 2 || row[1] == nil 
+    if row[1].chinese?
+        if row[1].traditional_chinese?
+            puts row[1]
+            outAry.push(row[1])
+        end
+    end
+end
+
+File.open(options.output,'w') do |f| 
+    f << outAry.map{|words| words}.join("\n")       # Dump to output file
+end 
 
